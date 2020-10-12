6bf911908e49cf766346bb639c0556b0dfce78f1
benjamin-sylvester <benjamin.sylvester@uvm.edu>
2020-10-12 17:45:45 -0400

    table class

---
+++ Table.py
@@ -0,0 +1,20 @@
+class Table:
+
+
+    def __init__(self, num_seats, location, is_free):
+        self.num_seats = num_seats
+        self.location = location
+        self.is_free = is_free
+
+
+    def get_is_free(self):
+        return self.is_free
+
+    def get_num_seats(self):
+        return self.num_seats
+
+    def get_location(self):
+        return self.location
+
+    def clear(self):
+        self.is_free = True
