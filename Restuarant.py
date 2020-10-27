7db6c4a59bdc76aa436700fa824eb83fe8de03ed
jmee1023 <jmmee@uvm.edu>
2020-10-27 16:32:53 -0400

    First implemntation of Welcome menu

--- Restuarant.py
+++ Restuarant.py
@@ -1,4 +1,24 @@
 
 
+def welcome_menu():
+    print("Welcome to Ben and Jimmy's Shrimp Shack by the Sea")
+    print("Please fill in the following information, and we will have you seated right away")
+    party_name = input("What is the name of your party: ")
+    party_size = input("How many people are in your party: ")
+    time = input("Would you like to be seated now (press 1) or later (press2)")
+    print("Please wait while we check our availability ")
+
+
+
+
+
+
+def main():
+    welcome_menu()
+
+main()
+
+
+
 
 
